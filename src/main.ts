bbd44c5b4ffa5c98f469121329c15ff2211a4721
akhzamov <abdulmalikakhzamov1@gmail.com>
2024-07-24 01:05:53 +0500

    edited

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://icebergbtl.uz', 'https://ice-berg-xi.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://icebergbtl.uz'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
